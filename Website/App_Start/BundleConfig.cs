d0a9138920e0e00f87ec863beaacb05f2d980cf6
Romain Parage <rparage@talentsoft.com>
2014-07-09 18:02:47 +0200

    Implement the website structure

---
+++ Website/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace Website
+{
+    using System.Web.Optimization;
+
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Content/scripts").Include(
+                "~/Content/scripts/script.js"));
+
+            bundles.Add(new StyleBundle("~/Content/styles").Include(
+                "~/Content/styles/site.css"));
+        }
+    }
+}
