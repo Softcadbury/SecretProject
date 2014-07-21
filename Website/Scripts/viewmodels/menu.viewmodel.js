4bc9f66511d53d22d34d9e899be885b157f6358b
softcadbury <romain.parage@gmail.com>
2014-07-21 23:56:22 +0200

    Create application structure and remove js tests

--- Website/Scripts/viewmodels/menu.viewmodel.js
+++ Website/Scripts/viewmodels/menu.viewmodel.js
@@ -3,33 +3,5 @@
     function (userDataservice) {
         return function () {
             var self = this;
-
-            self.user = ko.observable();
-            self.users = ko.observableArray();
-
-            // Test get
-            userDataservice.get(1).done(function (user) {
-                self.user(user);
-
-                // Test add
-                userDataservice.add(self.user).done(function (user) {
-                    console.log(user)
-                });
-
-                // Test update
-                userDataservice.update(self.user().id, self.user).done(function (user) {
-                    console.log(user)
-                });
-            });
-
-            // Test getAll
-            userDataservice.getAll(1).done(function (users) {
-                self.users(users);
-            });
-
-            // Test remove
-            userDataservice.remove(1).done(function () {
-                console.log('removed');
-            });
         };
     });
