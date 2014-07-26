82ba5480de4bc42679b17b1c8d42a881d926087f
softcadbury <romain.parage@gmail.com>
2014-07-26 23:29:04 +0200

    Add a test

---
+++ Tests/Controllers/TestUserController.cs
@@ -0,0 +1,38 @@
+﻿namespace Tests.Controllers
+{
+    using Controller.ApiControllers;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Repository.Models;
+    using System.Web.Http;
+    using System.Web.Http.Results;
+
+    [TestClass]
+    public class TestUserController
+    {
+        private UserController _userController;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _userController = new UserController();
+        }
+
+        [TestMethod]
+        public void UserController_Get_ExistingUser_Success()
+        {
+            // Arrange
+            string userName = "Softcadbury";
+            int userId = 1;
+
+            // Act
+            IHttpActionResult result = _userController.Get(userId);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkResult));
+
+            OkNegotiatedContentResult<User> negotiatedResult = result as OkNegotiatedContentResult<User>;
+            Assert.IsNotNull(negotiatedResult);
+            Assert.AreEqual<string>(userName, negotiatedResult.Content.Name);
+        }
+    }
+}
