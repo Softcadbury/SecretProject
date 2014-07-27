9512591f363a5ac5998b2a5227a61d7e67f9ebf5
softcadbury <romain.parage@gmail.com>
2014-07-27 18:23:37 +0200

    Modify menu depending on routing

--- Website/Scripts/controllers/MenuCtrl.js
+++ Website/Scripts/controllers/MenuCtrl.js
@@ -1,13 +1,17 @@
 ﻿(function () {
     angular
         .module('app')
-        .controller('MenuCtrl', ['$scope', 'UserFactory', MenuCtrl]);
+        .controller('MenuCtrl', ['$scope', '$location', 'UserFactory', MenuCtrl]);
 
-    function MenuCtrl($scope, UserFactory) {
+    function MenuCtrl($scope, $location, UserFactory) {
         $scope.userName = '';
 
         UserFactory.get(1).success(function (user) {
             $scope.userName = user.Name;
         })
+
+        $scope.getClass = function (path) {
+            return $location.path() == path ? 'active' : '';
+        }
     }
 })();
