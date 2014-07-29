bd0c96a2d2d16f4d5c8e917f0bf1957e02cb2fea
Romain Parage <rparage@talentsoft.com>
2014-07-29 18:45:13 +0200

    Create an helper for the culture

---
+++ Website/Helpers/CultureHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace Website.Helpers
+{
+    using System.Threading;
+
+    /// <summary>
+    /// Helper for the application culture
+    /// </summary>
+    public static class CultureHelper
+    {
+        private static readonly string CurrentCulture = Thread.CurrentThread.CurrentUICulture.Name.ToLowerInvariant();
+
+        /// <summary>
+        /// Check if the parameter equals the current culture
+        /// </summary>
+        public static bool IsCurrent(string culture)
+        {
+            return culture == CurrentCulture;
+        }
+    }
+}
