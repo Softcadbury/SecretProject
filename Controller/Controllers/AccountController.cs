f404467771213d408cb0d92e31e883d9b0c97a55
Romain Parage <rparage@talentsoft.com>
2014-07-30 18:15:42 +0200

    Add account and preview controllers

---
+++ Controller/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿namespace Controller.Controllers
+{
+    using System.Web.Mvc;
+    using Infrastructure.BaseClasses;
+
+    /// <summary>
+    /// Account controller
+    /// </summary>
+    public class AccountController : BaseController
+    {
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        public ActionResult Connection()
+        {
+            return View();
+        }
+
+        public ActionResult ValidateRegistration()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult ValidateConnection()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Controller/Controllers/PreviewController.cs
@@ -0,0 +1,16 @@
+﻿namespace Controller.Controllers
+{
+    using System.Web.Mvc;
+    using Infrastructure.BaseClasses;
+
+    /// <summary>
+    /// Preview controller
+    /// </summary>
+    public class PreviewController : BaseController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
