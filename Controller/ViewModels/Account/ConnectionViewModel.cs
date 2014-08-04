bdcd766a1208f18ee66e9fbd71b9301318463651
Romain Parage <rparage@talentsoft.com>
2014-08-04 19:04:07 +0200

    Add two view models for account

---
+++ Controller/ViewModels/Account/ConnectionViewModel.cs
@@ -0,0 +1,23 @@
+﻿namespace Controller.ViewModels.Account
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Resources;
+
+    /// <summary>
+    /// Connection
+    /// </summary>
+    public class ConnectionViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Account_Email", ResourceType = typeof(Resource))]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Account_Password", ResourceType = typeof(Resource))]
+        public string Password { get; set; }
+    }
+}
---
+++ Controller/ViewModels/Account/RegistrationViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Controller.ViewModels.Account
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Resources;
+
+    /// <summary>
+    /// Registration
+    /// </summary>
+    public class RegistrationViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Account_Email", ResourceType = typeof(Resource))]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Account_Password", ResourceType = typeof(Resource))]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Account_ConfirmPassword", ResourceType = typeof(Resource))]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
