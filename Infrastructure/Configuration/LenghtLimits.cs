6fd65764e432d7d21fed38860072cf7d30149355
Romain Parage <rparage@talentsoft.com>
2014-08-07 18:35:10 +0200

    Add class to fix minimum and maximum lenght fro stings

---
+++ Infrastructure/Configuration/LenghtLimits.cs
@@ -0,0 +1,14 @@
+﻿namespace Infrastructure.Configuration
+{
+    /// <summary>
+    /// Represents limits lenght for strings
+    /// </summary>
+    public static class LenghtLimits
+    {
+        public const int UserNameMaxLenght = 20;
+        public const int UserNameMinLenght = 6;
+
+        public const int PasswordMaxLenght = 50;
+        public const int PasswordMinLenght = 6;
+    }
+}
