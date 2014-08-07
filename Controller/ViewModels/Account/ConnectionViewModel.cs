089f6523bab7298c7f9150feeab8f1ef057986bd
Romain Parage <rparage@talentsoft.com>
2014-08-07 18:36:54 +0200

    Use LenghtLimits and add user name for connection

--- Controller/ViewModels/Account/ConnectionViewModel.cs
+++ Controller/ViewModels/Account/ConnectionViewModel.cs
@@ -2,6 +2,8 @@
 {
     using System.ComponentModel.DataAnnotations;
 
+    using Infrastructure.Configuration;
+
     using Resources;
 
     /// <summary>
@@ -10,12 +12,12 @@
     public class ConnectionViewModel
     {
         [Required(ErrorMessageResourceName = "Account_FieldRequiredError", ErrorMessageResourceType = typeof(Resource))]
-        [EmailAddress(ErrorMessageResourceName = "Account_FieldValidationError", ErrorMessageResourceType = typeof(Resource), ErrorMessage = null)]
-        [Display(Name = "Account_Email", ResourceType = typeof(Resource))]
-        public string Email { get; set; }
+        [StringLength(LenghtLimits.UserNameMaxLenght, MinimumLength = LenghtLimits.UserNameMinLenght, ErrorMessageResourceName = "Account_FieldMinimumLengthError", ErrorMessageResourceType = typeof(Resource))]
+        [Display(Name = "Account_UserName", ResourceType = typeof(Resource))]
+        public string UserName { get; set; }
 
         [Required(ErrorMessageResourceName = "Account_FieldRequiredError", ErrorMessageResourceType = typeof(Resource))]
-        [StringLength(100, MinimumLength = 6, ErrorMessageResourceName = "Account_FieldMinimumLengthError", ErrorMessageResourceType = typeof(Resource))]
+        [StringLength(LenghtLimits.PasswordMaxLenght, MinimumLength = LenghtLimits.PasswordMinLenght, ErrorMessageResourceName = "Account_FieldMinimumLengthError", ErrorMessageResourceType = typeof(Resource))]
         [DataType(DataType.Password)]
         [Display(Name = "Account_Password", ResourceType = typeof(Resource))]
         public string Password { get; set; }
