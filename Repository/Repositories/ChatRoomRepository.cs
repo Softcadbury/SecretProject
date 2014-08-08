48bfd83bb090edd82eccab27619ba9b727f0f199
softcadbury <romain.parage@gmail.com>
2014-08-08 23:13:11 +0200

    Add ChatRoom service and repository

---
+++ Repository/Repositories/ChatRoomRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace Repository.Repositories
+{
+    using Infrastructure.BaseClasses;
+
+    using Repository.Contexts;
+    using Repository.Models;
+
+    /// <summary>
+    /// ChatRoom repository
+    /// </summary>
+    public class ChatRoomRepository : BaseRepository<ChatRoom>
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ChatRoomRepository()
+            : base(new SecretProjectContext())
+        {
+        }
+    }
+}
---
+++ Service/Services/ChatRoomService.cs
@@ -0,0 +1,44 @@
+﻿namespace Service.Services
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Infrastructure.BaseClasses;
+    using Infrastructure.Services.Requests;
+    using Infrastructure.Services.Responses;
+
+    using Repository.Models;
+    using Repository.Repositories;
+
+    /// <summary>
+    /// ChatRoom service
+    /// </summary>
+    public class ChatRoomService : BaseService<ChatRoom, ChatRoomRepository>
+    {
+        private static readonly ChatRoomRepository ChatRoomRepository = new ChatRoomRepository();
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ChatRoomService()
+            : base(ChatRoomRepository)
+        {
+        }
+
+        /// <summary>
+        /// Get a model
+        /// </summary>
+        public new Response<ChatRoom> Get(GetRequest request)
+        {
+            return base.Get(request);
+        }
+
+        /// <summary>
+        /// Get a list of models
+        /// </summary>
+        public new Response<List<ChatRoom>> GetPage(GetPageRequest request)
+        {
+            return base.GetPage(request);
+        }
+    }
+}
