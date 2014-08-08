127e1686a0c5d0bb94a124f2264fe4e35eff18df
softcadbury <romain.parage@gmail.com>
2014-08-08 23:34:27 +0200

    Add controller and factory for chatroom

---
+++ Controller/ApiControllers/ChatRoomController.cs
@@ -0,0 +1,43 @@
+﻿namespace Controller.ApiControllers
+{
+    using System.Collections.Generic;
+    using System.Web.Http;
+    using Infrastructure.BaseClasses;
+    using Infrastructure.Services.Requests;
+    using Infrastructure.Services.Responses;
+    using Repository.Models;
+    using Service.Services;
+
+    /// <summary>
+    /// ChatRoom controller
+    /// </summary>
+    [RoutePrefix("api/chatrooms")]
+    public class ChatRoomController : BaseApiController
+    {
+        private readonly ChatRoomService chatRoomService;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ChatRoomController()
+        {
+            chatRoomService = new ChatRoomService();
+        }
+
+        // GET: /api/chatrooms?pageIndex=0
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult GetPage([FromUri] int pageIndex)
+        {
+            if (pageIndex < 0)
+            {
+                return BadRequest("");
+            }
+
+            var request = new GetPageRequest(pageIndex, 20);
+            Response<List<ChatRoom>> response = chatRoomService.GetPage(request);
+
+            return RenderResponse(response);
+        }
+    }
+}
---
+++ Website/Scripts/factories/ChatRoomFactory.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    angular
+        .module('app')
+        .factory('ChatRoomFactory', ['$http', ChatRoomFactory]);
+
+    function ChatRoomFactory($http) {
+        var urlBase = '/api/chatrooms';
+        var factory = {};
+
+        factory.getPage = function (pageIndex) {
+            return $http.get(urlBase + '/?pageIndex=' + pageIndex);
+        };
+
+        return factory;
+    }
+})();
