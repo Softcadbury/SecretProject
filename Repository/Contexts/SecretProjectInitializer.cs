cf9079161745c0a9b8b80929cc169912afd1d18d
softcadbury <romain.parage@gmail.com>
2014-08-09 13:18:54 +0200

    Display chat rooms

--- Repository/Contexts/SecretProjectInitializer.cs
+++ Repository/Contexts/SecretProjectInitializer.cs
@@ -15,12 +15,25 @@
         /// </summary>
         protected override void Seed(SecretProjectContext context)
         {
+            // Initialize users
             var users = new List<User>
             {
-                new User { UserName = "Softcadbury" },
+                new User { UserName = "Softcadbury" }
             };
 
             users.ForEach(s => context.Users.Add(s));
+
+            // Initialize chat rooms
+            var chatRooms = new List<ChatRoom>
+            {
+                new ChatRoom { Title = "Video Games" },
+                new ChatRoom { Title = "Sport" },
+                new ChatRoom { Title = "Cinema" },
+                new ChatRoom { Title = "Dating" }
+            };
+
+            chatRooms.ForEach(s => context.ChatRooms.Add(s));
+
             context.SaveChanges();
         }
     }
--- Website/Scripts/controllers/ChatRoomsCtrl.js
+++ Website/Scripts/controllers/ChatRoomsCtrl.js
@@ -1,8 +1,13 @@
 ﻿(function () {
     angular
         .module('app')
-        .controller('ChatRoomsCtrl', ['$scope', ChatRoomsCtrl]);
+        .controller('ChatRoomsCtrl', ['$scope', 'ChatRoomFactory', ChatRoomsCtrl]);
 
-    function ChatRoomsCtrl($scope) {
+    function ChatRoomsCtrl($scope, ChatRoomFactory) {
+        $scope.chatRooms = [];
+
+        ChatRoomFactory.getPage(1).success(function (chatRooms) {
+            $scope.chatRooms = chatRooms;
+        });
     }
 })();
