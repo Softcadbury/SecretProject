de7ee358130bb97bb0e9120d3f24d42cc56907fc
Romain Parage <rparage@talentsoft.com>
2014-08-12 13:36:40 +0200

    Add signalR to the project

---
+++ Controller/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿namespace Controller.Hubs
+{
+    using Microsoft.AspNet.SignalR;
+    using Microsoft.AspNet.SignalR.Hubs;
+
+    /// <summary>
+    /// Chat hub
+    /// </summary>
+    [HubName("ChatHub")]
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
---
+++ Website/Startup.cs
@@ -0,0 +1,17 @@
+﻿[assembly: Microsoft.Owin.OwinStartup(typeof(Website.Startup))]
+
+namespace Website
+{
+    using Owin;
+
+    /// <summary>
+    /// Class used at the application startup
+    /// </summary>
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
