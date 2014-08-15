1f4549e9aa26924ae997215fca6b72fb9fd79ecf
softcadbury <romain.parage@gmail.com>
2014-08-15 19:28:09 +0200

    Chat the signalR service to a factory

--- Website/Scripts/controllers/ChatRoomsCtrl.js
+++ Website/Scripts/controllers/ChatRoomsCtrl.js
@@ -1,10 +1,10 @@
 ﻿(function () {
     angular
         .module('app')
-        .controller('ChatRoomsCtrl', ['$scope', 'SignalRService', 'ChatRoomFactory', ChatRoomsCtrl]);
+        .controller('ChatRoomsCtrl', ['$scope', 'SignalRFactory', 'ChatRoomFactory', ChatRoomsCtrl]);
 
-    function ChatRoomsCtrl($scope, SignalRService, ChatRoomFactory) {
-        SignalRService.initialize();
+    function ChatRoomsCtrl($scope, SignalRFactory, ChatRoomFactory) {
+        SignalRFactory.initialize();
         $scope.chatRooms = [];
 
         ChatRoomFactory.getPage(1).success(function (chatRooms) {
---
+++ Website/Scripts/factories/SignalRFactory.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    angular
+        .module('app')
+        .factory('SignalRFactory', SignalRFactory);
+
+    function SignalRFactory() {
+        var factory = {};
+
+        factory.initialize = function () {
+            var chat = $.connection.chatHub;
+
+            $.connection.hub.start().done(function () {
+                window.chatMessageTest = function (name, message) {
+                    chat.server.send(name, message);
+                };
+            });
+
+            chat.client.broadcastMessage = function (name, message) {
+                alert(name + message);
+            };
+        };
+
+        return factory;
+    }
+})();
