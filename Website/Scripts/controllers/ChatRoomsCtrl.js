0c039dfdccc1a2e63be593822c5b2450162ef733
softcadbury <romain.parage@gmail.com>
2014-08-17 12:29:44 +0200

    Some work on chat

--- Website/Scripts/controllers/ChatRoomsCtrl.js
+++ Website/Scripts/controllers/ChatRoomsCtrl.js
@@ -1,14 +1,25 @@
 ﻿(function () {
     angular
         .module('app')
-        .controller('ChatRoomsCtrl', ['$scope', 'SignalRFactory', 'ChatRoomFactory', ChatRoomsCtrl]);
+        .controller('ChatRoomsCtrl', ['$scope', 'ChatRoomFactory', ChatRoomsCtrl]);
 
-    function ChatRoomsCtrl($scope, SignalRFactory, ChatRoomFactory) {
-        SignalRFactory.initialize();
+    function ChatRoomsCtrl($scope, ChatRoomFactory) {
+        var chatHub = $.connection.chatHub;
+        $.connection.hub.start();
+
         $scope.chatRooms = [];
+        $scope.messages = [];
 
         ChatRoomFactory.getPage(1).success(function (chatRooms) {
             $scope.chatRooms = chatRooms;
         });
+
+        $scope.send = function () {
+            chatHub.server.send('romain', 'lola');
+        };
+
+        chatHub.client.broadcastMessage = function (name, message) {
+            $scope.messages.push({ name: name, message: message });
+        };
     }
 })();
