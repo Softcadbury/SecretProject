d566579ddf241c9388d0e75bf5849aec837240e9
softcadbury <romain.parage@gmail.com>
2014-08-23 15:03:39 +0200

    Make the selection possible for the list of chat rooms

--- Website/Scripts/controllers/ChatRoomsCtrl.js
+++ Website/Scripts/controllers/ChatRoomsCtrl.js
@@ -7,16 +7,26 @@
         var chatHub = $.connection.chatHub;
 
         $scope.chatRooms = [];
+        $scope.selectedRoomId = null;
+        $scope.userName = $rootScope.currentUser ? $rootScope.currentUser.UserName : '';
         $scope.messages = [];
         $scope.newMessage = '';
-        $scope.userName = $rootScope.currentUser ? $rootScope.currentUser.UserName : '';
-
-        $scope.$on('currentUser.updated', function () {
-            $scope.userName = $rootScope.currentUser.UserName;
-        });
 
         ChatRoomFactory.getPage(1).success(function (chatRooms) {
             $scope.chatRooms = chatRooms;
+            $scope.selectedRoomId = $scope.chatRooms[0].Id;
+        });
+
+        $scope.getClass = function (chatRoom) {
+            return chatRoom.Id === $scope.selectedRoomId ? 'active' : '';
+        }
+
+        $scope.changeRoom = function (chatRoom) {
+            $scope.selectedRoomId = chatRoom.Id;
+        }
+
+        $scope.$on('currentUser.updated', function () {
+            $scope.userName = $rootScope.currentUser.UserName;
         });
 
         chatHub.client.broadcastMessage = function (user, message) {
@@ -27,7 +37,7 @@
 
         $.connection.hub.start().done(function () {
             $scope.send = function () {
-                if ($scope.newMessage.trim() == '') {
+                if ($scope.newMessage.trim() === '') {
                     return;
                 }
 
