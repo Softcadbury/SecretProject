80133f2f81084658fe565d7fcd065c371cbcfda7
softcadbury <romain.parage@gmail.com>
2014-08-24 20:20:25 +0200

    Add a change password view model

---
+++ Service/ViewModels/Account/ChangePasswordViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Service.ViewModels.Account
+{
+    using Infrastructure.Configuration;
+    using Resources;
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Change password
+    /// </summary>
+    public class ChangePasswordViewModel
+    {
+        [Required(ErrorMessageResourceName = "Account_FieldRequiredError", ErrorMessageResourceType = typeof(Resource))]
+        [StringLength(LenghtLimits.PasswordMaxLenght, MinimumLength = LenghtLimits.PasswordMinLenght, ErrorMessageResourceName = "Account_FieldMinimumLengthError", ErrorMessageResourceType = typeof(Resource))]
+        [DataType(DataType.Password)]
+        [Display(Name = "Account_NewPassword", ResourceType = typeof(Resource))]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessageResourceName = "Account_FieldRequiredError", ErrorMessageResourceType = typeof(Resource))]
+        [StringLength(LenghtLimits.PasswordMaxLenght, MinimumLength = LenghtLimits.PasswordMinLenght, ErrorMessageResourceName = "Account_FieldMinimumLengthError", ErrorMessageResourceType = typeof(Resource))]
+        [DataType(DataType.Password)]
+        [Display(Name = "Account_ActualPassword", ResourceType = typeof(Resource))]
+        public string ActualPassword { get; set; }
+
+        [Compare("ActualPassword", ErrorMessageResourceName = "Account_PasswordMatchingError", ErrorMessageResourceType = typeof(Resource))]
+        [DataType(DataType.Password)]
+        [Display(Name = "Account_PasswordConfirmation", ResourceType = typeof(Resource))]
+        public string ConfirmPassword { get; set; }
+    }
+}
