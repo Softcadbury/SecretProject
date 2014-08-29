fe0b5a9a5df785bf8aa4ebfcc23f44b42a35750d
softcadbury <romain.parage@gmail.com>
2014-08-30 00:44:59 +0200

    Add alert in setting forms

--- Website/Scripts/controllers/SettingsCtrl.js
+++ Website/Scripts/controllers/SettingsCtrl.js
@@ -7,6 +7,11 @@
         $scope.userName = $rootScope.currentUser ? $rootScope.currentUser.UserName : '';
         $scope.userEmail = $rootScope.currentUser ? $rootScope.currentUser.Email : '';
 
+        $scope.userUpdateSuccess = false;
+        $scope.userUpdateError = false;
+        $scope.passwordUpdateSuccess = false;
+        $scope.passwordUpdateError = false;
+
         // Event fired when the current user is updated
         $rootScope.$on('currentUser.updated', function () {
             $scope.userName = $rootScope.currentUser.UserName;
@@ -17,20 +22,29 @@
         $scope.saveChanges = function () {
             $rootScope.currentUser.UserName = $scope.userName;
             $rootScope.currentUser.Email = $scope.userEmail;
-            $rootScope.$broadcast('currentUser.updated');
 
-            // todo: check errors
-            UserFactory.update($rootScope.currentUser);
+            UserFactory.update($rootScope.currentUser).
+                success(function () {
+                    $scope.userUpdateSuccess = true;
+                    $rootScope.$broadcast('currentUser.updated');
+                }).
+                error(function () {
+                    $scope.userUpdateError = true;
+                });
         };
 
         // Function to save the change of user's password
         $scope.savePassword = function () {
-            // todo: check errors
-            UserFactory.updatePassword($rootScope.currentUser, $scope.actualPassword, $scope.newPassword, $scope.passwordConfirmation);
-
-            $scope.actualPassword = '';
-            $scope.newPassword = '';
-            $scope.passwordConfirmation = '';
+            UserFactory.updatePassword($rootScope.currentUser, $scope.actualPassword, $scope.newPassword, $scope.passwordConfirmation).
+                success(function () {
+                    $scope.passwordUpdateSuccess = true;
+                    $scope.actualPassword = '';
+                    $scope.newPassword = '';
+                    $scope.passwordConfirmation = '';
+                }).
+                error(function () {
+                    $scope.passwordUpdateError = true;
+                });
         };
 
         // Function to delete the user's account
