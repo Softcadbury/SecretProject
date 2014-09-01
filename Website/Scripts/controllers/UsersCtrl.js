03012c0c6cd1807bec43de4328df5d73dea5869d
softcadbury <romain.parage@gmail.com>
2014-09-01 21:40:12 +0200

    Improves design for the display of users

--- Website/Scripts/controllers/UsersCtrl.js
+++ Website/Scripts/controllers/UsersCtrl.js
@@ -5,10 +5,22 @@
 
     function UsersCtrl($scope, UserFactory) {
         $scope.users = [];
+        $scope.selectedUserId = null;
 
         // Get the first page of users
         UserFactory.getPage(1).success(function (users) {
             $scope.users = users;
+            $scope.selectedUserId = $scope.users[0].Id;
         });
+
+        // Function to know if the user is the selected user
+        $scope.isActive = function (user) {
+            return user.Id === $scope.selectedUserId;
+        }
+
+        // Function to change the selected user
+        $scope.changeUser = function (user) {
+            $scope.selectedUserId = user.Id;
+        }
     }
 })(angular);
