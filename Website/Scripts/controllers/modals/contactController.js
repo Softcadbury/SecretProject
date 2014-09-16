6d642087a8b585dbdf24b8a55ca13e918d3fae38
softcadbury <romain.parage@gmail.com>
2014-09-16 22:40:05 +0200

    Add the send message for the contact modal

--- Website/Scripts/controllers/modals/contactController.js
+++ Website/Scripts/controllers/modals/contactController.js
@@ -6,5 +6,11 @@
         .controller('contactController', ['$rootScope', '$scope', contactController]);
 
     function contactController($rootScope, $scope) {
+        $scope.message = '';
+
+        // Function to send the message of contact
+        $scope.sendMessage = function (clickEvent) {
+            $(clickEvent.target).button('loading');
+        }
     }
 })(angular);
