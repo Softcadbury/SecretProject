4bb1c012841a49538b82803e870743ac4ad08a9d
softcadbury <romain.parage@gmail.com>
2014-09-25 20:58:04 +0200

    First part of the modal to delete the account

---
+++ Website/Scripts/controllers/modals/deleteAccountConfirmationController.js
@@ -0,0 +1,27 @@
+﻿(function (angular) {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('deleteAccountConfirmationController', ['$rootScope', '$scope', '$sce', 'bootstrapHelperService', 'userFactory', deleteAccountConfirmationController]);
+
+    function deleteAccountConfirmationController($rootScope, $scope, $sce, bootstrapHelperService, userFactory) {
+        $scope.deleteAccountError = $sce.trustAsHtml('');
+
+        // Function to delete the user's account
+        $scope.deleteAccount = function (clickEvent) {
+            bootstrapHelperService.buttonToLoadingState(clickEvent);
+
+            userFactory.removeCurrent()
+                .success(function () {
+                    location.reload();
+                })
+                .error(function (data) {
+                    $scope.deleteAccountError = $sce.trustAsHtml(data.Message);
+                })
+                .finally(function () {
+                    bootstrapHelperService.resetButtonState(clickEvent);
+                });
+        };
+    }
+})(angular);
